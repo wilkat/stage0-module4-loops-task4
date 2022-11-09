9bdf1c260e791174f05760baa3a72aa57e74a132
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-09 22:40:46 +0100

    new

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -9,7 +9,7 @@ public class NumberToBreakOn {
             }
         } else {
             for(int i = 1; i <= numberToGoUntil; i++) {
-                if(i == toBreakWith) {
+                if(i > toBreakWith) {
                     break;
                 }
                 System.out.println(i);
